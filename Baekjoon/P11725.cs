2b875f4983a41ad33fb3e4af8566a655de3f19ec
Gmum <eelsig1@naver.com>
2026-03-29 15:33:04 +0900

    두문제품

---
+++ Baekjoon/P11725.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baekjoon
+{
+    internal class P11725
+    {
+        /*
+         
+         문제
+
+            루트 없는 트리가 주어진다. 이때, 트리의 루트를 1이라고 정했을 때, 각 노드의 부모를 구하는 프로그램을 작성하시오.
+
+            입력
+            첫째 줄에 노드의 개수 N (2 ≤ N ≤ 100,000)이 주어진다. 둘째 줄부터 N-1개의 줄에 트리 상에서 연결된 두 정점이 주어진다.
+
+            출력
+            첫째 줄부터 N-1개의 줄에 각 노드의 부모 노드 번호를 2번 노드부터 순서대로 출력한다.
+
+         */
+
+
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            // 노드의 개수 N
+            // 간선의 개수 N-1
+
+            List<int>[] fromto = new List<int>[N + 1];
+            Queue<int> que = new Queue<int>();
+            int[] answ = new int[N + 1];
+            for (int i = 0; i < N-1; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+                int a = int.Parse(input[0]);
+                int b = int.Parse(input[1]);
+
+                if (a == 1)
+                {
+                    que.Enqueue(b);
+                    answ[b] = 1;
+                }
+                else if (b == 1)
+                {
+                    que.Enqueue(a);
+                    answ[a] = 1;
+                }
+
+                if (fromto[a] == null) fromto[a] = new List<int>();
+                fromto[a].Add(b);
+
+                if (fromto[b] == null) fromto[b] = new List<int>();
+                fromto[b].Add(a);
+            }
+
+            bool[] isConnected = new bool[N + 1];
+            isConnected[1] = true;
+
+            while(que.Count > 0)
+            {
+                int par = que.Dequeue();
+                isConnected[par] = true;
+
+                if (fromto[par] != null)
+                    foreach(var c in fromto[par])
+                    {
+                        if (isConnected[c]) 
+                            continue;
+                        answ[c] = par;
+                        que.Enqueue(c);
+                    }
+            }
+
+            StringBuilder buf = new StringBuilder();
+            for (int i = 2; i < N + 1; i++)
+            {
+                buf.Append(answ[i]); buf.Append('\n');
+            }
+            Console.WriteLine(buf);
+            
+            
+            
+        }
+    }
+}
---
+++ Baekjoon/P15654.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baekjoon
+{
+    internal class P15654
+    {
+        /*
+         
+         
+         
+         
+         
+         
+         */
+
+        static StringBuilder buf = new StringBuilder();
+        static bool isVisited(int[] row, int depth, int n)
+        {
+            for(int i = 0; i < row.Length && i < depth; i++)
+            {
+                if (row[i] == n) return true;
+            }
+            return false;
+        }
+        static void Render(int[] row, int M)
+        {
+            for(int i = 0; i < M; i++)
+            {
+                buf.Append(row[i]);
+                buf.Append(" ");
+            }
+            buf.Append('\n');
+        }
+        static void Track(int[] row, int[] arr, int N, int M, int depth)
+        {
+            if (depth == M)
+            {
+                Render(row, M);
+                return;
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                if (isVisited(row, depth, arr[i])) continue;
+                
+                row[depth] = arr[i];
+
+                Track(row, arr, N, M, depth + 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            string[] input = Console.ReadLine().Split(' ');
+            int N = int.Parse(input[0]);
+            int M = int.Parse(input[1]);
+
+
+            int[] arr = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+
+            // N <= 8
+            Array.Sort(arr);
+            int[] row = new int[N];
+            HashSet<int> visited = new HashSet<int>();
+
+            Track(row, arr, N, M, 0);
+
+            Console.WriteLine(buf);
+
+        }
+    }
+}
