331b896b2d4bb38a0887b360131578ab8a20af78
김용오 <gmumdev@gmail.com>
2026-04-01 17:41:10 +0900

    tst

---
+++ Baekjoon/P1629.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baekjoon
+{
+	internal class P1629
+	{
+
+
+		static void Main(string[] args)
+		{
+			string[] input = Console.ReadLine().Split(' ');
+
+			int A, B, C;
+			A = int.Parse(input[0]);
+			B = int.Parse(input[1]);
+			C = int.Parse(input[2]);
+
+			// R = (A * B) % C, Q = (A * B) / C
+			// (A * B) = C * Q + R 의 R
+			// R = (A * B) - C * Q
+		}
+	}
+}
